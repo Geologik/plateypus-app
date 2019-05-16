bd175c2f596f29aa5e6d6c832ea6b9c225a375ed
Kristian Thy <krt@sigmaestimates.com>
2019-05-16 13:12:43 +0200

    Test main app

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,27 @@
 """Test the Plateypus app."""
 
+from functools import partial
+
+from kivy.clock import Clock
+
+from plateypus import main
+
+
+def stop_kivy(app, *args):
+    """Stop Kivy application.
+
+    See https://github.com/KeyWeeUsr/KivyUnitTest#writing-unit-test-for-kivy-application
+    """
+    app.stop()
+
 
 def test_tautology():
     """This test canary always passes."""
     assert 2 + 2 == 4
+
+
+def test_app_build():
+    """Verify that the app can be built and started."""
+    app = main.PlateypusApp()
+    Clock.schedule_once(partial(stop_kivy, app), 0.000001)
+    app.run()
