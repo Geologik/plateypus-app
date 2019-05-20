d1deb3435d3b10f16fc1f87132ae9ba43bc7c09c
Kristian Thy <krt@sigmaestimates.com>
2019-05-20 14:37:46 +0200

    Add deps, fix debug script

--- plateypus/__init__.py
+++ plateypus/__init__.py
@@ -4,6 +4,6 @@ from sys import argv
 
 __clean_args = []  # pylint: disable=invalid-name
 for arg in argv[1:]:
-    if not "--cov" in arg:
+    if not "--cov" in arg and arg not in ("--exitfirst", "--pdb"):
         __clean_args.append(arg)
 argv[1:] = __clean_args
